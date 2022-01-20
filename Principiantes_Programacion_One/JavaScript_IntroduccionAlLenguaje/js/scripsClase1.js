87a3188baa85c1c07e739271f62dec3601d07607
lmoreno98 <lucho_moreno@live.com.ar>
2022-01-20 17:42:30 -0300

    Clase 1 de javascript utilizar document.getContent

---
+++ Principiantes_Programacion_One/JavaScript_IntroduccionAlLenguaje/js/scripsClase1.js
@@ -0,0 +1,10 @@
+// Podemos llamar al h1 con su nombre de class poniendo .(nombre en el class)
+var titulo = document.querySelector(".titulo");
+
+// Reviso el valor del titulo con textContent
+console.log(titulo.textContent);
+
+// Cambio el titulo
+titulo.textContent = "Buena Vida Nuticion"
+
+console.log("Estoy imprimiendo desde scripts.js")
