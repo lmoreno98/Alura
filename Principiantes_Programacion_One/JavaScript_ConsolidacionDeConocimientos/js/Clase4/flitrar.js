9c5e0406e5f5eaa35db6ef6d8507f816b6a45b57
lmoreno98 <lucho_moreno@live.com.ar>
2022-01-25 12:34:49 -0300

    Codigo para filtrar los pacientes por el nombre

---
+++ Principiantes_Programacion_One/JavaScript_ConsolidacionDeConocimientos/js/Clase4/flitrar.js
@@ -0,0 +1,37 @@
+var campoFiltro = document.querySelector("#filtrar-tabla");
+
+campoFiltro.addEventListener("input",function(){
+
+    var pacientes = document.querySelectorAll(".paciente");
+
+
+    if(this.value.length > 0){
+        for(var i = 0; i < pacientes.length; i ++)
+        {
+            var paciente = pacientes[i];
+            var tdNombre = paciente.querySelector(".info-nombre");
+            var nombre = tdNombre.textContent;
+
+            // Diferenciacion de mayusculas y minusculas
+            var expresion = new RegExp(this.value, "i" );
+    
+            // Con expresion.test nos filtra los valores nuevos ingresados
+            if(!expresion.test(nombre)){
+                paciente.classList.add("invisible");
+            }
+            else{
+                paciente.classList.remove("invisible");
+            }
+            
+        }
+
+    }
+    else{
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i]
+            paciente.classList.remove("invisible")
+        }
+    }
+
+
+});
