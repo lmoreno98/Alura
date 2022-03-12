177911b333c43f1cf1ef31c21c943757c05ac543
lmroeno98 <lucho_moreno@live.com.ar>
2022-03-12 19:24:32 -0300

    Creacion de la clase cliente

---
+++ Front-End/JavaScript/HerenciaEInterfaz/Cliente.js
@@ -0,0 +1,14 @@
+/* Creacion de la clase cliente */
+export class Cliente {
+    nombreCliente;
+    dniCliente;
+    rutCliente;
+
+    /* Con esto podemos inicializar los valores como si fuera una funcion */
+    constructor(nombreCliente, dniCliente, rutCliente) {
+        this.nombreCliente = nombreCliente;
+        this.dniCliente = dniCliente;
+        this.rutCliente = rutCliente;
+
+    }
+}
