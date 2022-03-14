87172f2e00883ea38b57f89d85f354da15f65a54
lmroeno98 <lucho_moreno@live.com.ar>
2022-03-14 13:14:13 -0300

    Creacion del autenticador de usuarios

---
+++ Front-End/JavaScript/HerenciaEInterfaz/SistemaAutenticacion.js
@@ -0,0 +1,12 @@
+export class SistemaAutenticacion {
+    static login(usuario, clave) {
+        if ("autenticable" in usuario 
+        && usuario.autenticable instanceof Function) {
+            return usuario.autenticable(clave)
+        }
+        else{
+            return false;
+        }
+
+    }
+}
