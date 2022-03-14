8b03a5620dbb8837b72ff5a8ff8aabdced487e71
lmroeno98 <lucho_moreno@live.com.ar>
2022-03-14 13:15:07 -0300

    Creacion de una clave para los clientes para poder validar la autenticacion

--- Front-End/JavaScript/HerenciaEInterfaz/Cliente.js
+++ Front-End/JavaScript/HerenciaEInterfaz/Cliente.js
@@ -3,12 +3,20 @@ export class Cliente {
     nombreCliente;
     dniCliente;
     rutCliente;
-
+    #clave;
     /* Con esto podemos inicializar los valores como si fuera una funcion */
     constructor(nombreCliente, dniCliente, rutCliente) {
         this.nombreCliente = nombreCliente;
         this.dniCliente = dniCliente;
         this.rutCliente = rutCliente;
+        this.#clave = ""
+    }
 
+    asignarClave(clave) {
+        this.#clave = clave;
+    }
+
+    autenticable(clave) {
+        return true;
     }
 }
