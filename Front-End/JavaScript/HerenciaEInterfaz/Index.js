2f3adb5a45564c7793e478f00d069e3ed12bd31c
lmroeno98 <lucho_moreno@live.com.ar>
2022-03-14 13:13:50 -0300

    Probar nuevas funciones creadas como la autenicacion, creacion de empleados

--- Front-End/JavaScript/HerenciaEInterfaz/Index.js
+++ Front-End/JavaScript/HerenciaEInterfaz/Index.js
@@ -1,7 +1,13 @@
 /* <importacion de clases */
 import { Cliente } from './Cliente.js';
-import { CuentaAhorro } from './CuentaAhorro.js'
-import { CuentaCorriente } from './CuentaCorriente.js'
+import { CuentaAhorro } from './Cuentas/CuentaAhorro.js'
+import { CuentaCorriente } from './Cuentas/CuentaCorriente.js'
+import { Cuenta } from "./Cuentas/Cuenta.js"
+import { CuentaNomina } from './Cuentas/CuentaNomina.js';
+import { Empleado } from './Empleados/Empleado.js';
+import { Director } from './Empleados/Director.js';
+import { Gerente } from './Empleados/Gerente.js'
+import { SistemaAutenticacion } from './SistemaAutenticacion.js'
 
 const cliente = new Cliente("Luciano", "40980849", "123456");
 const cuentaDeLuciano = new CuentaCorriente("1", "001", cliente);
@@ -10,20 +16,18 @@ const cliente2 = new Cliente("Juan", "38932693", "548796");
 const cuentaDeJuan = new CuentaCorriente("2", "002", cliente2);
 
 const cuentaAhorroLuciano = new CuentaAhorro("9985", "001", cliente, 0);
+const cuentaNominaLuciano = new CuentaNomina(cliente, "314", "001");
 
 
-console.log(cuentaDeLuciano);
-cuentaDeLuciano.depositoEnCuenta(150);
-console.log(cuentaDeLuciano.verSaldo());
-cuentaDeLuciano.retirarDeCuenta(70);
-console.log(cuentaDeLuciano.verSaldo());
-console.log(cuentaDeLuciano);
+const empleado = new Empleado("Juan Perez", "2145679", 10000);
+empleado.asignarClave("123456");
+const gerente = new Gerente("Juan Gomez", "84661614", 12000);
+gerente.asignarClave("789456");
+//const director = new Director("Juan Gonzalez", "746464648", 15000);
 
+console.log(SistemaAutenticacion.login(empleado, "123456"));
+console.log(SistemaAutenticacion.login(gerente, "789456"));
 
-console.log("----------------------------------------")
-console.log(cuentaAhorroLuciano);
-cuentaAhorroLuciano.depositoEnCuenta(200);
-console.log(cuentaAhorroLuciano.verSaldo());
-cuentaAhorroLuciano.retirarDeCuenta(120);
-console.log(cuentaAhorroLuciano.verSaldo());
-console.log(cuentaAhorroLuciano);
+const cliente3 = new Cliente("Jorge", "15467988", "123456");
+cliente3.asignarClave("01111");
+console.log(SistemaAutenticacion.login(cliente3, "01111"));
