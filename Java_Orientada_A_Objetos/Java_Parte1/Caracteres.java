8f6299bc3ea0b72976d8cce76bc3d9a0d87ee612
lmroeno98 <lucho_moreno@live.com.ar>
2022-05-26 18:20:52 -0300

    Manejo de caracteres

---
+++ Java_Orientada_A_Objetos/Java_Parte1/Caracteres.java
@@ -0,0 +1,16 @@
+public class Caracteres {
+    public static void main(String[] args) {
+        
+        // Char
+        char caracter = 65;
+        System.out.println(caracter);
+
+        caracter = 'a';
+        System.out.println(caracter);
+        
+        // String
+        String palabra = "Hola mundo!";
+        System.out.println(palabra);
+
+    }
+}
