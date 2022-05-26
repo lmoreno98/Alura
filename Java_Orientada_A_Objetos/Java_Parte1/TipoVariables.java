10bf540b26ff2856c136310bc0f0681e080cb161
lmroeno98 <lucho_moreno@live.com.ar>
2022-05-26 18:20:38 -0300

    Tipos de datos

---
+++ Java_Orientada_A_Objetos/Java_Parte1/TipoVariables.java
@@ -0,0 +1,24 @@
+public class TipoVariables {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+
+        // Asignacion de variables
+
+        // Int
+        int edad = 24;
+        System.out.println(edad);
+
+        edad = 27;
+        System.out.println(edad);
+
+        System.out.println("Mi edad es " + edad);
+
+        // Doubles
+        double salario = 1250.50;
+        System.out.println(salario);
+
+        // Casteo
+        int salarioInt = (int) salario;
+        System.out.println(salarioInt);
+    }
+}
