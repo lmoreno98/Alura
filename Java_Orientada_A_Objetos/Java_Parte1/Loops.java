78880b38db2934df4d898228ee2f193ef3fbdc8d
lmroeno98 <lucho_moreno@live.com.ar>
2022-05-26 18:20:26 -0300

    Aprendiendo loops

---
+++ Java_Orientada_A_Objetos/Java_Parte1/Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    
+    public static void main(String[] args) {
+        
+        int contador = 1;
+
+        // Bucle while igual a c
+        while(contador <= 10){
+            System.out.println(contador);
+            contador++;
+        }
+        
+
+        System.out.println("Bucle for");
+        // Bucle for 
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
