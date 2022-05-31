321c4fac14e4da533eebb6161f6413d35439d97b
lmroeno98 <lucho_moreno@live.com.ar>
2022-05-31 18:20:20 -0300

    Creacion de objeto cuenta con sus metodos

---
+++ Java_Orientada_A_Objetos/Java_Parte2OrientacionAObjetos/Cuenta.java
@@ -0,0 +1,33 @@
+package Java_Parte2OrientacionAObjetos;
+
+class Cuenta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public boolean retirar(double valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean transferir(double valor, Cuenta cuenta ){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            cuenta.depositar(valor);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
